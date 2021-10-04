fa7b940fb00a24e1706120f9b18d8b2d03a4bfed
tangguoliang <tangguoliang@qutoutiao.net>
2021-10-04 21:03:29 +0800

    init

---
+++ src/main/java/com/example/cxbackend/util/CommonUtil.java
@@ -0,0 +1,8 @@
+package com.example.cxbackend.util;
+
+/**
+ *
+ */
+public class CommonUtil {
+
+}
---
+++ src/test/java/com/example/cxbackend/CxBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cxbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CxBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
